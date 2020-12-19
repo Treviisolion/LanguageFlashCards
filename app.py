b1c302021848ff5d572ceac4c9b426be1676e3a9
Trevyn <trevyncase@gmail.com>
2020-12-18 18:34:09 -0600

    Added framework for connecting to database

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 """Language Flashcard application"""
 
 from flask import Flask, render_template
+from models import db, connect_db
 import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'JohnathonAppleseed452')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///flashcards')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
-# connect_db(app)
-
-# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'JohnathonAppleseed452')
-
-# db.create_all()
+connect_db(app)
+db.create_all()
 
 @app.route('/')
 def get_main_page():
