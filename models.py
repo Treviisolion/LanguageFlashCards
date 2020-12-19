5da837e7c7f898c876159035a68a6553c4dd3f24
Trevyn <trevyncase@gmail.com>
2020-12-18 20:21:59 -0600

    Performed minor formatting

--- models.py
+++ models.py
@@ -6,11 +6,13 @@ from flask_bcrypt import Bcrypt
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 
+
 def connect_db(app):
     """Connect to database."""
 
     db.app = app
     db.init_app(app)
+
 
 class User(db.Model):
     """Users"""
@@ -40,6 +42,7 @@ class User(db.Model):
         else:
             return False
 
+
 class Word(db.Model):
     """Foreign language morphemes, ideally base form of words"""
 
@@ -47,11 +50,14 @@ class Word(db.Model):
     __table_args__ = (db.UniqueConstraint('user', 'word', 'pronunciation'),)
 
     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    user = db.Column(db.String(), db.ForeignKey('users.username'), nullable=False)
+    user = db.Column(db.String(),
+                     db.ForeignKey('users.username'),
+                     nullable=False)
     word = db.Column(db.String(), nullable=False)
     pronunciation = db.Column(db.String(), nullable=False)
 
     synonyms = db.relationship('Synonym', cascade="all, delete-orphan")
+
 
 class Synonym(db.Model):
     """English language synonyms, i.e. any plausible translation for the given word"""
@@ -59,4 +65,6 @@ class Synonym(db.Model):
     __tablename__ = 'synonyms'
 
     synonym = db.Column(db.String(), primary_key=True)
-    word_id = db.Column(db.Integer(), db.ForeignKey('words.id'), primary_key=True)
+    word_id = db.Column(db.Integer(),
+                        db.ForeignKey('words.id'),
+                        primary_key=True)
